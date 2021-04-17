160f5365a01185025f86b427243897139857c1d6
andrea chaparro <andrea.chaparro@est.iudigital.edu.co>
2021-04-16 21:36:58 -0500

    Borrado carpeta

--- src/main/java/co/edu/iudigital/app/model/repository/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package co.edu.iudigital.app.model.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import co.edu.iudigital.app.model.entity.Movie;
-
-public interface MovieRepository extends CrudRepository<Movie, Integer>{
-}
--- src/main/java/co/edu/iudigital/app/model/repository/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-
-package co.edu.iudigital.app.model.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import co.edu.iudigital.app.model.entity.Profile;
-
-public interface ProfileRepository extends CrudRepository<Profile, Integer>{
-}
